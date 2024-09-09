b732600e0b2c71e3f7dd7cc569886fa60ffce972
Avgurin <avgurin@list.ru>
2024-09-09 17:24:37 +1100

    Switch color

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ test_box1.addEventListener("click", handleClick);
 
 function handleClick(event) {
   //const enteredDigit = parseInt(event.target.id.substring(4), 10);
-  test_box1.className = "test_box_red";
+  console.log(event);
+  if(event.target.className=="test_box_red"){
+    event.target.className = "test_box_black";
+  } else {
+    event.target.className = "test_box_red";
+  }
 }
